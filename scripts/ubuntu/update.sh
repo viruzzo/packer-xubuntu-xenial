726c52cb3b862d9e537d10bb6bdc3251153518b8
Zeno Grandi <zeno.grandi@pleiade.it>
2016-10-06 13:50:29 +0200

    Added libcurl3 package

--- scripts/ubuntu/update.sh
+++ scripts/ubuntu/update.sh
@@ -28,9 +28,9 @@ apt-get -y dist-upgrade;
 # zgrandi: install Xubuntu without most extra packages
 apt-get -y install --no-install-recommends xubuntu-desktop \
 	acpi-support curl desktop-file-utils evince file-roller firefox fonts-liberation \
-	indicator-application indicator-messages indicator-sound laptop-detect libnotify-bin\
-	light-locker lightdm-gtk-greeter-settings menulibre mousepad pavucontrol ristretto \
-	ttf-ubuntu-font-family update-notifier xfce4-indicator-plugin xfce4-power-manager \
+	indicator-application indicator-messages indicator-sound laptop-detect libcurl3 \
+	libnotify-bin light-locker lightdm-gtk-greeter-settings menulibre mousepad pavucontrol \
+	ristretto ttf-ubuntu-font-family update-notifier xfce4-indicator-plugin xfce4-power-manager \
 	xfce4-quicklauncher-plugin xfce4-terminal xfce4-volumed xfce4-whiskermenu-plugin;
 
 reboot;
