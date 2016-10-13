13e11f7d9737315ae390c4360a077b2d1099791b
Zeno Grandi <zeno.grandi@pleiade.it>
2016-10-13 17:51:11 +0200

    Moved scripts to Xubuntu specific directory; Set locale to UTF-8; Added gvfs and some xfce4 plugins

--- scripts/xubuntu/update.sh
+++ scripts/xubuntu/update.sh
@@ -26,12 +26,41 @@ EOF
 apt-get -y dist-upgrade;
 
 # zgrandi: install Xubuntu without most extra packages
-apt-get -y install --no-install-recommends xubuntu-desktop \
-	acpi-support curl desktop-file-utils evince file-roller firefox fonts-liberation \
-	indicator-application indicator-messages indicator-sound laptop-detect libcurl3 \
-	libnotify-bin light-locker lightdm-gtk-greeter-settings menulibre mousepad pavucontrol \
-	ristretto ttf-ubuntu-font-family update-notifier xfce4-indicator-plugin xfce4-power-manager \
-	xfce4-quicklauncher-plugin xfce4-terminal xfce4-volumed xfce4-whiskermenu-plugin;
+apt-get	-y install --no-install-recommends xubuntu-desktop \
+	acpi-support \
+	apport-gtk \
+	curl \
+	desktop-file-utils \
+	evince \
+	file-roller \
+	firefox \
+	fonts-liberation \
+	gvfs \
+	indicator-application \
+	indicator-messages \
+	indicator-sound \
+	laptop-detect \
+	libcurl3 \
+	libnotify-bin \
+	light-locker \
+	lightdm-gtk-greeter-settings \
+	menulibre \
+	mousepad \
+	pavucontrol \
+	ristretto \
+	thunar-archive-plugin \
+	ttf-ubuntu-font-family \
+	update-notifier \
+	xfce4-datetime-plugin \
+	xfce4-indicator-plugin \
+	xfce4-mount-plugin \
+	xfce4-power-manager \
+	xfce4-quicklauncher-plugin \
+	xfce4-screenshooter \
+	xfce4-taskmanager \
+	xfce4-terminal \
+	xfce4-volumed \
+	xfce4-whiskermenu-plugin;
 
 reboot;
 sleep 60;
