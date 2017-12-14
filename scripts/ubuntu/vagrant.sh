ddafb4756bbef9c9c8fded9ff4c2e815ab89fe62
Zeno Grandi <zeno.grandi@pleiade.it>
2017-12-14 17:09:20 +0100

    Try curl before wget to download the Vagrant public key

--- scripts/ubuntu/vagrant.sh
+++ scripts/ubuntu/vagrant.sh
@@ -2,10 +2,10 @@
 
 pubkey_url="https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub";
 mkdir -p $HOME_DIR/.ssh;
-if command -v wget >/dev/null 2>&1; then
-    wget --no-check-certificate "$pubkey_url" -O $HOME_DIR/.ssh/authorized_keys;
-elif command -v curl >/dev/null 2>&1; then
+if command -v curl >/dev/null 2>&1; then
     curl --insecure --location "$pubkey_url" > $HOME_DIR/.ssh/authorized_keys;
+elif command -v wget >/dev/null 2>&1; then
+    wget --no-check-certificate "$pubkey_url" -O $HOME_DIR/.ssh/authorized_keys;
 else
     echo "Cannot download vagrant public key";
     exit 1;
